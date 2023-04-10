2a7879534252e75999f4a7492a22e4d0ccda4c59
Christoph Korn <christoph.korn@posteo.de>
2023-04-10 18:37:55 +0200

    Only run the program once
    Give control to the new version and exit the old version.

--- HomeOffice/Program.cs
+++ HomeOffice/Program.cs
@@ -3,6 +3,25 @@ using System.Linq;
 using System.IO;
 using System.Collections.Generic;
 const string FileName = "homeoffice.csv";
+
+Mutex runningMutex = new(true, "Global\\ckorn.HomeOffice", out bool runningMutexCreated);
+Mutex? updateMutex = null;
+bool updateMutexCreated = false;
+const string UpdateMutexNamespace="Global\\ckorn.HomeOffice.update";
+if(!runningMutexCreated)
+{
+	Console.WriteLine("Application already running");
+	updateMutex = new(true, UpdateMutexNamespace, out updateMutexCreated);
+	if(!updateMutexCreated)
+	{
+		throw new InvalidOperationException($"{nameof(updateMutexCreated)}=False");
+	}
+	Console.WriteLine("Waiting for running mutex...");
+	runningMutex.WaitOne();
+	Console.WriteLine("Got running mutex");
+	updateMutex.ReleaseMutex();
+	updateMutex.Dispose();
+}
 Dictionary<DateTime, bool> homeOfficeDayDictionary = ReadFile();
 //foreach(KeyValuePair<DateTime, bool> keyValuePair in homeOfficeDayDictionary)
 //{
@@ -10,18 +29,36 @@ Dictionary<DateTime, bool> homeOfficeDayDictionary = ReadFile();
 //}
 DateTime today = DateTime.Today;
 Console.WriteLine($"{today:d}");
-if(!homeOfficeDayDictionary.ContainsKey(today))
+if(!homeOfficeDayDictionary.TryGetValue(today, out bool homeOffice))
 {
 	string answer=string.Empty;
 	do
 	{
 		Console.WriteLine("HomeOffice? (J/N)");
-		answer=Console.ReadLine() ?? string.Empty;
-		answer=answer.ToUpper();
+		try
+		{
+			answer=Reader.ReadLine(5000) ?? string.Empty;
+			answer=answer.ToUpper();
+		}
+		catch (TimeoutException)
+		{
+			updateMutex = new(false, UpdateMutexNamespace, out updateMutexCreated);
+			updateMutex.Dispose();
+			if(!updateMutexCreated)
+			{
+				Console.WriteLine("Release running mutex");
+				runningMutex.ReleaseMutex();
+				runningMutex.Dispose();
+				return;
+			}
+		}
 	}while((answer != "J") && (answer != "N"));
-	bool homeOffice=(answer == "J");
+	homeOffice=(answer == "J");
 	AddEntry(today, homeOffice);
 }
+Console.WriteLine($"HomeOffice={homeOffice}");
+runningMutex.ReleaseMutex();
+runningMutex.Dispose();
 
 static Dictionary<DateTime, bool> ReadFile()
 {
